43eaee3b9f7cf7d4c5107a94fd5a4bd621367358
katiek27 <katiek5412@gmail.com>
2023-09-12 11:02:08 -0700

    edited and added another bash command

--- _posts/2023-09-11-PairShowcase-Bash.md
+++ _posts/2023-09-11-PairShowcase-Bash.md
@@ -13,5 +13,5 @@ Observe the image, and an example of it running.
 <br>
 <img width="700" alt="Screenshot 2023-09-11 at 11 58 28 AM" src="https://github.com/katiek27/katies2/assets/142449577/1800ab99-a902-4cb0-9fd5-fbaebac47e0c">
 
-I learned about bash and creating a new file. It's called a script and it's able to run multiple commands at once.
+I learned about bash and creating a new file. It's called a script and it's able to run multiple commands at once. This is my old commmand, check out the new post for an updated version.
 
--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-# It's asking your age !
 
-echo "What is your age?"
+#!/bin/bash
+# A script that asks for your age and provides some responses based on your age.
+
+echo "How old are you?"
 read age
-echo "wow, you look a lot younger than $age years old!"
+
+if [[ $age -lt 18 ]]; then
+  echo "You're quite young! Enjoy your youth!"
+elif [[ $age -ge 18 && $age -lt 30 ]]; then
+  echo "You're in your twenties. Life is an adventure!"
+elif [[ $age -ge 30 && $age -lt 50 ]]; then
+  echo "You're in your thirties or forties. The prime of life!"
+else
+  echo "You're experienced! Wisdom comes with age."
+fi
+
+echo "By the way, you look great for $age years old!"
